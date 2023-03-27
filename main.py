0dc1f69c7eb1addabae0c4e79d92fc78287662e4
SergeiVorobev <vorobevse86@gmail.com>
2023-03-27 22:50:14 +0200

    refactor: add post model, update of main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
 
-from db import Base, engine, SessionLocal
+from db import Base, engine, SessionLocal, User, Post
 
 # This creates the tables in the database, if they don't already exist.
 Base.metadata.create_all(bind=engine)
@@ -16,7 +17,14 @@ def get_db():
     finally:
         db.close()
 
-# This creates a FastAPI endpoint that returns "Hello, world!".
+# This creates a FastAPI endpoint that returns all users and their posts.
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
+def read_users(db: Session = Depends(get_db)):
+    users = db.query(User).all()
+    results = []
+    for user in users:
+        posts = []
+        for post in user.posts:
+            posts.append({"title": post.title, "content": post.content})
+        results.append({"id": user.id, "username": user.username, "email": user.email, "posts": posts})
+    return {"users": results}
